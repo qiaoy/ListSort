8e76d58f6cc399a086b4003e52f84616d431e38c
乔岩 <qiaoyan@hujiang.com>
2017-03-31 13:35:04 +0800

    add QYTitleListViewModel

---
+++ ListSort/ViewModels/QYTitleListViewModel.h
@@ -0,0 +1,15 @@
+//
+//  QYTitleListViewModel.h
+//  ListSort
+//
+//  Created by 乔岩 on 2017/3/31.
+//  Copyright © 2017年 qiaoyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QYTitleListViewModel : NSObject
+
+@property (nonatomic, readonly, copy) NSArray *list;
+
+@end
---
+++ ListSort/ViewModels/QYTitleListViewModel.m
@@ -0,0 +1,27 @@
+//
+//  QYTitleListViewModel.m
+//  ListSort
+//
+//  Created by 乔岩 on 2017/3/31.
+//  Copyright © 2017年 qiaoyan. All rights reserved.
+//
+
+#import "QYTitleListViewModel.h"
+#import "QYFileManager.h"
+
+@interface QYTitleListViewModel ()
+
+@property (nonatomic, copy) NSArray *list;
+
+@end
+
+@implementation QYTitleListViewModel
+
+- (instancetype)init {
+    if (self = [super init]) {
+        self.list = [QYFileManager currentTitleList];
+    }
+    return self;
+}
+
+@end
