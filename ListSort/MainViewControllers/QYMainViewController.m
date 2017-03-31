49271e681720adbff2891c23e24549dfa6e8f314
乔岩 <qiaoyan@hujiang.com>
2017-03-31 16:52:03 +0800

    tableView

--- ListSort/MainViewControllers/QYMainViewController.m
+++ ListSort/MainViewControllers/QYMainViewController.m
@@ -8,18 +8,24 @@
 
 #import <Masonry/Masonry.h>
 #import "QYMainViewController.h"
+#import "QYTitleListViewModel.h"
 
 @interface QYMainViewController () <UITableViewDataSource>
+
+@property (nonatomic, strong) QYTitleListViewModel *listViewModel;
 
 @property (nonatomic, strong) UITableView *tableView;
 
 @end
+
+static NSString *const kTitleListCell = @"kTitleListCell";
 
 @implementation QYMainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = NSStringFromClass([self class]);
+    self.listViewModel = [[QYTitleListViewModel alloc] init];
     [self setUpUI];
 }
 
@@ -35,11 +41,13 @@
 #pragma mark - UITableViewDataSource Delegate
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+    return self.listViewModel.list.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kTitleListCell forIndexPath:indexPath];
+    cell.textLabel.text = self.listViewModel.list[indexPath.row];
+    return cell;
 }
 
 #pragma mark - Getters
@@ -47,6 +55,8 @@
 - (UITableView *)tableView {
     if (!_tableView) {
         _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableView.dataSource = self;
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kTitleListCell];
     }
     return _tableView;
 }
