5c0c3523d7bed3e4a674988f3b33cfb14e65eef4
乔岩 <qiaoyan@hujiang.com>
2017-04-05 22:16:03 +0800

    config tableView

--- ListSort/ListSortViewController/QYListSortViewController.m
+++ ListSort/ListSortViewController/QYListSortViewController.m
@@ -6,6 +6,7 @@
 //  Copyright © 2017年 qiaoyan. All rights reserved.
 //
 
+#import <Masonry/Masonry.h>
 #import "QYListSortViewController.h"
 
 @interface QYListSortViewController () <UITableViewDataSource, UITableViewDelegate>
@@ -13,10 +14,14 @@
 @property (nonatomic, copy) NSArray<NSString *> *titleList;
 
 @property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UIButton *footerBtn;
 
 @end
 
 static NSString *const kTitleListCell = @"kTitleListCell";
+static NSString *const kFooterBtnTitle = @"恢复默认排序";
+
+static CGFloat const kFooterBtnH = 60;
 
 @implementation QYListSortViewController
 
@@ -31,6 +36,7 @@ static NSString *const kTitleListCell = @"kTitleListCell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationController.navigationBar.backgroundColor = [UIColor redColor];
     self.title = NSStringFromClass([self class]);
     [self setUpUI];
 }
@@ -39,24 +45,38 @@ static NSString *const kTitleListCell = @"kTitleListCell";
 
 - (void)setUpUI {
     [self.view addSubview:self.tableView];
+    [self.tableView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+}
+
+#pragma mark - Events
+
+- (void)footerBtnClick:(UIButton *)button {
+    
 }
 
 #pragma mark - UITableViewDataSource Delegate
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
-}
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 0;
+    return self.titleList.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kTitleListCell forIndexPath:indexPath];
+    cell.textLabel.text = self.titleList[indexPath.row];
+    return cell;
 }
 
 #pragma mark - UITableView Delegate
 
+- (__kindof UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    return self.footerBtn;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return kFooterBtnH;
+}
 
 #pragma mark - Getters
 
@@ -68,6 +88,17 @@ static NSString *const kTitleListCell = @"kTitleListCell";
         [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kTitleListCell];
     }
     return _tableView;
+}
+
+- (UIButton *)footerBtn {
+    if (!_footerBtn) {
+        _footerBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        _footerBtn.bounds = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, kFooterBtnH);
+        [_footerBtn setTitle:kFooterBtnTitle forState:UIControlStateNormal];
+        [_footerBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_footerBtn addTarget:self action:@selector(footerBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _footerBtn;
 }
 
 @end
