c50ffb69df12030fa53f3b478add6aa1b0196381
乔岩 <qiaoyan@hujiang.com>
2017-04-06 14:33:09 +0800

    add title list cell

---
+++ ListSort/Views/QYTitleListTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  QYTitleListTableViewCell.h
+//  ListSort
+//
+//  Created by 乔岩 on 2017/4/6.
+//  Copyright © 2017年 qiaoyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QYTitleListTableViewCell : UITableViewCell
+
+- (void)configTitleListTableViewCellWithTitle:(NSString *)title;
+
+@end
---
+++ ListSort/Views/QYTitleListTableViewCell.m
@@ -0,0 +1,51 @@
+//
+//  QYTitleListTableViewCell.m
+//  ListSort
+//
+//  Created by 乔岩 on 2017/4/6.
+//  Copyright © 2017年 qiaoyan. All rights reserved.
+//
+
+#import <Masonry/Masonry.h>
+#import "QYTitleListTableViewCell.h"
+
+@interface QYTitleListTableViewCell ()
+
+@property (nonatomic, strong) UILabel *titleLabel;
+
+@end
+
+static CGFloat kPadding = 12.f;
+
+@implementation QYTitleListTableViewCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        [self setUpUI];
+    }
+    return self;
+}
+
+- (void)setUpUI {
+    [self.contentView addSubview:self.titleLabel];
+    [self.titleLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.contentView).insets(UIEdgeInsetsMake(kPadding, kPadding, kPadding, kPadding));
+    }];
+}
+
+- (void)configTitleListTableViewCellWithTitle:(NSString *)title {
+    self.titleLabel.text = title;
+}
+
+#pragma mark - Getters
+
+- (UILabel *)titleLabel {
+    if (!_titleLabel) {
+        _titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        _titleLabel.textColor = [UIColor blackColor];
+        _titleLabel.font = [UIFont systemFontOfSize:16.f];
+    }
+    return _titleLabel;
+}
+
+@end
